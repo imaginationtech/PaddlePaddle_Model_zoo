8049cc3f41025eb3ebf948da4e3a3e2706a275da
jiansowa <james.wjs@hotmail.com>
2022-07-05 17:21:34 +0800

    Configuration works at Jiali office

--- tools/run_grpc_client.sh
+++ tools/run_grpc_client.sh
@@ -2,4 +2,4 @@ ScriptsL=$(dirname $0)
 
 export PYTHONPATH=${ScriptsL}/../python:$PYTHONPATH
 
-python ${ScriptsL}/../python/engine/backend/pvr_grpc/pvr_infer_client.py
+python ${ScriptsL}/../python/engine/backend/pvr_grpc/pvr_infer_test_client.py
