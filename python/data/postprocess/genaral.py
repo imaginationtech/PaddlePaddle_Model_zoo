7c12b1e60e24f764a62c6130729cd21373a8d7d1
Zhuoran Lv <Zhuoran.Lv@imgtec.com>
2023-02-06 10:05:15 +0800

    Add Poinpillars model and related preprocess and postprocess

--- python/data/postprocess/genaral.py
+++ python/data/postprocess/genaral.py
@@ -88,3 +88,79 @@ class ToKittRecord(OpBase):
             kwargs[k] = np.array(data)
 
         return kwargs
+
+
+@op_register
+class VoxelNetDecoder(OpBase):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+    def __call__(self, **kwargs):
+        anchors_mask = np.load(self.kwargs['anchors_mask'])
+        anchors = np.load(self.kwargs['anchors'])
+        box_preds = kwargs['box_preds'].reshape([1, 107136, 7])
+        cls_preds = kwargs['cls_preds'].reshape([1, 107136, 1])
+        dir_preds = kwargs['dir_preds'].reshape([1, 107136, 2])
+        boxes,scores,labels = self.single_post_process(box_preds, cls_preds, dir_preds, anchors_mask,anchors)
+        boxes_2d = np.concatenate((boxes[:,:2],boxes[:,3:5],boxes[:,6:]),axis=-1)
+        kwargs['bboxes'] = boxes_2d
+        return kwargs
+        
+    def single_post_process(self, box_preds, cls_preds, dir_preds, anchors_mask,anchors):
+
+        box_preds = self.second_box_decode_numpy(box_preds, anchors)
+        box_preds = box_preds.squeeze(0)
+        cls_preds = cls_preds.squeeze(0)
+        dir_preds = dir_preds.squeeze(0).reshape((-1,2))
+
+        # _single_post_process
+        box_preds = box_preds[anchors_mask]
+        cls_preds = cls_preds[anchors_mask]
+        cls_confs=1/(1+(np.exp((-cls_preds))))
+
+        cls_scores = cls_confs.max(-1)
+        cls_labels = cls_confs.argmax(-1)
+
+        kept = cls_scores >= 0.05
+        dir_preds = dir_preds[anchors_mask]
+        dir_labels = dir_preds.argmax(axis=-1)
+
+        box_preds = box_preds[kept]
+        cls_scores = cls_scores[kept]
+        cls_labels = cls_labels[kept]
+        dir_labels = dir_labels[kept]
+
+        #_box_not_empty
+        box_preds[:, 2] = box_preds[:, 2] + box_preds[:, 5] * 0.5
+        for i in range(len(box_preds)):
+            if box_preds[i,6] > 0:
+                box_preds[i,6] += np.pi
+        #rotate_nms_pcdet 
+        box_preds = box_preds[:, [0, 1, 2, 4, 3, 5, -1]]
+        box_preds[:, -1] = -box_preds[:, -1] - np.pi / 2
+        order = cls_scores.argsort(0)[::-1]
+        order = order[:1000]
+        box_preds = box_preds[order]
+        box_preds = box_preds.reshape([-1, 7])
+        return box_preds, cls_scores, cls_labels
+    
+    
+    def second_box_decode_numpy(self, encodings, anchors):
+        """
+        Decode 3D bboxes for VoxelNet/PointPillars.
+        Args:
+            encodings ([N, 7] Tensor): encoded boxes: x, y, z, w, l, h, r
+            anchors ([N, 7] Tensor): anchors
+        """
+        xa, ya, za, wa, la, ha, ra = np.split(anchors, 7, axis=-1)
+        xt, yt, zt, wt, lt, ht, rt = np.split(encodings, 7, axis=-1)
+
+        diagonal = np.sqrt(la**2 + wa**2)
+        xg = xt * diagonal + xa
+        yg = yt * diagonal + ya
+        zg = zt * ha + za
+
+        lg = np.exp(lt) * la
+        wg = np.exp(wt) * wa
+        hg = np.exp(ht) * ha
+        rg = rt + ra
+        return np.concatenate([xg, yg, zg, wg, lg, hg, rg], axis=-1)
--- python/data/preprocess/others.py
+++ python/data/preprocess/others.py
@@ -63,3 +63,78 @@ class ComputeDownRatio(OpBase):
         result = {"down_ratios": down_ratios}
         kwargs.update(result)
         return kwargs
+
+@op_register
+class HardVoxelize(OpBase):
+    def __init__(self, point_cloud_range, voxel_size, max_points_in_voxel,
+                 max_voxel_num):
+        self.point_cloud_range = point_cloud_range
+        self.voxel_size = voxel_size
+        self.max_points_in_voxel = max_points_in_voxel
+        self.max_voxel_num = max_voxel_num
+
+    def __call__(self, points, **kwargs):
+        num_points, num_point_dim = points.shape[0:2]
+        self.point_cloud_range = np.array(self.point_cloud_range)
+        self.voxel_size = np.array(self.voxel_size)
+        voxels = np.zeros((self.max_voxel_num, self.max_points_in_voxel, num_point_dim),
+                        dtype=points.dtype)
+        coords = np.zeros((self.max_voxel_num, 3), dtype=np.int32)
+        num_points_per_voxel = np.zeros((self.max_voxel_num, ), dtype=np.int32)
+        grid_size = np.round((self.point_cloud_range[3:6] - self.point_cloud_range[0:3]) /
+                            self.voxel_size).astype('int32')
+
+        grid_size_x, grid_size_y, grid_size_z = grid_size
+
+        grid_idx_to_voxel_idx = np.full((grid_size_z, grid_size_y, grid_size_x),
+                                        -1,
+                                        dtype=np.int32)
+
+        num_voxels = self._points_to_voxel(points, self.voxel_size, self.point_cloud_range,
+                                    grid_size, voxels, coords,
+                                    num_points_per_voxel, grid_idx_to_voxel_idx,
+                                    self.max_points_in_voxel, self.max_voxel_num)
+
+        voxels = voxels[:num_voxels]
+        coords = coords[:num_voxels]
+        num_points_per_voxel = num_points_per_voxel[:num_voxels]
+        coords = (np.append(np.zeros((coords.shape[0],1)), coords, axis=-1)).astype('float32')
+        num_points_per_voxel = num_points_per_voxel.astype('float32')
+        result = {"coords": coords[:,2:],
+                  "num_points_per_voxel":num_points_per_voxel,
+                  "voxels":voxels}
+        kwargs.update(result)
+        return kwargs
+
+    def _points_to_voxel(self, points, voxel_size, point_cloud_range, grid_size, voxels,  
+                     coords, num_points_per_voxel, grid_idx_to_voxel_idx,
+                     max_points_in_voxel, max_voxel_num):
+        num_voxels = 0
+        num_points = points.shape[0]
+        # x, y, z
+        coord = np.zeros(shape=(3, ), dtype=np.int32)
+
+        for point_idx in range(num_points):
+            outside = False
+            for i in range(3):
+                coord[i] = np.floor(
+                    (points[point_idx, i] - point_cloud_range[i]) / voxel_size[i])
+                if coord[i] < 0 or coord[i] >= grid_size[i]:
+                    outside = True
+                    break
+            if outside:
+                continue
+            voxel_idx = grid_idx_to_voxel_idx[coord[2], coord[1], coord[0]]
+            if voxel_idx == -1:
+                voxel_idx = num_voxels
+                if num_voxels >= max_voxel_num:
+                    continue
+                num_voxels += 1
+                grid_idx_to_voxel_idx[coord[2], coord[1], coord[0]] = voxel_idx
+                coords[voxel_idx, 0:3] = coord[::-1]
+            curr_num_point = num_points_per_voxel[voxel_idx]
+            if curr_num_point < max_points_in_voxel:
+                voxels[voxel_idx, curr_num_point] = points[point_idx]
+                num_points_per_voxel[voxel_idx] = curr_num_point + 1
+
+        return num_voxels
